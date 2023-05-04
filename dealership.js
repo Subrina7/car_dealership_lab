940e619041156b36cd08f750b3efb3d49c974ef2
Subrina7 <Subrinaf.7@gmail.com>
2023-05-04 18:46:10 +0100

    final commit

--- dealership.js
+++ dealership.js
@@ -5,6 +5,9 @@ const Dealership = function (name, maxNoOfCars, carsInStock){
 
 }
 
+const { access } = require("fs");
+const Car = require(`./car`);
+
 Dealership.prototype.numberOfCarsInStock = function(){
     return this.carsInStock.length;
 }
@@ -13,6 +16,21 @@ Dealership.prototype.addStock = function(newCar){
     this.carsInStock.push(newCar);
 }
 
-module.exports = Dealership;
+Dealership.prototype.manufacturersName = function(){
+   const manufacturer = this.carsInStock.map(car  => car.manufacturer);
+   return manufacturer;
+}
+
+// Dealership.prototype.getCarByManufacturer = function(){
+//     const filteredCars = this.carsInStock.filter( cars => cars.manufacturer);
+//     return filteredCars;
+// }
+
+// Dealership.prototype.totalValue = function(){
+//     const totalVal = Car.reduce((accumulator, car.price) => {
+//     return accumulator + totalVal;
+//     },0)}
+
+// module.exports = Dealership;
 
 
--- dealership.test.js
+++ dealership.test.js
@@ -1,14 +1,17 @@
 const Dealership = require(`./dealership`);
-const Car = require(`./car`)
+const Car = require(`./car`);
+const exp = require("constants");
 
 let dealership1;
 let jag;
 beforeEach(() => {
     dealership1 = new Dealership("Auto Trader", 100)
-    jag = new Car("Land Rover", 70000, "V8");
-    range = new Car("Land Rover", 80000, "V8");
-    landRover = new Car("Land Rover", 80000, "V8");
+    jag = new Car("Land Rover", 10000, "V8");
+    range = new Car("Land Rover", 10000, "V8");
+    landRover = new Car("Land Rover", 10000, "V8");
+    ferrari = new Car("Ferrari", 10000, "Colombo");
     dealership1.addStock(jag);
+    dealership1.addStock(ferrari);
 })
 
 test("has name", () => {
@@ -26,19 +29,41 @@ test("has max number of cars", () => {
     expect(dealership1.maxNoOfCars).toBe(expected)
 })
 
-test("update max number of cars", () => {
-    dealership1.maxNoOfCar = 105;
-    expected = 105;
-    expect(dealership1.maxNoOfCars).toBe(105)
-})
+// test("update max number of cars", () => {
+//     dealership1.maxNoOfCar = 105;
+//     expected = 105;
+//     expect(dealership1.maxNoOfCars).toBe(105)
+// })
 
 test("check stock", () => {
     const expected = 1
-    expect(dealership1.numberOfCarsInStock()).toBe(1);
+    expect(dealership1.numberOfCarsInStock()).toBe(2);
 })
 
 test("Check add stock method", () =>{
     dealership1.addStock(landRover);
     dealership1.addStock(range);
-    expect(dealership1.numberOfCarsInStock()).toBe(3);
+    expect(dealership1.numberOfCarsInStock()).toBe(4);
 })
+
+// test("returns manufacturer", () =>{
+//     expect(jag.manufacturersName()).toBe("Land Rover");
+// })
+
+test("returns manufacturer", () =>{
+    expected = ["Land Rover", "Ferrari"]
+    actual = dealership1.manufacturersName()
+    expect(actual).toEqual(expected);
+})
+
+// test("filters car by manufacturer", () => {
+//     expected = [jag, range, landRover]
+//     actual = dealership1.getCarByManufacturer("Land Rover")
+//     expect(actual).toBe(expected);
+// })
+
+// test("adds total", () => {
+//     const expected = 40000;
+//     actual = dealership1.totalValue()
+//     expect(actual).toBe(expected);
+// })
