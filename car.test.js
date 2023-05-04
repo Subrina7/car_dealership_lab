0147e2ceddb56bc0a36646f6c7866ac07b4c3645
Subrina7 <Subrinaf.7@gmail.com>
2023-05-04 15:55:19 +0100

    car tests complete

--- car.test.js
+++ car.test.js
@@ -1,3 +1,4 @@
+const exp = require("constants");
 const Car = require(`./car`)
 
 let ferrari;
@@ -7,14 +8,34 @@ beforeEach(() => {
 
 })
 
-
 test('has manufacturer', () => {
     const expected = "Ferrari"
     expect(ferrari.manufacturer).toBe(expected);
 
-});
+})
 
 test('update manufacturer', () => {
     ferrari.manufacturer = "Volvo"
     expect(ferrari.manufacturer).toBe("Volvo")
+})
+
+test('has price', () => {
+    const expected = 250000
+    expect(ferrari.price).toBe(250000);
+
+})
+
+test('update price', () => {
+    ferrari.price = 200000
+    expect(ferrari.price).toBe(200000)
+})
+
+test('has engine', () => {
+    const expected = "Colombo"
+    expect(ferrari.engineType).toBe(expected)
+})
+
+test('update engine', () => {
+    ferrari.engineType = "V8"
+    expect(ferrari.engineType).toBe("V8")
 })
